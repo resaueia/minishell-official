2119c9c2e669e96d34379c28a70be9291701be11
Renan <renansaueia@gmail.com>
2024-06-07 18:41:02 -0300

    corrections to the get_envp function (not sure if it works)

--- envp.c
+++ envp.c
@@ -19,31 +19,6 @@ typedef struct s_envp
 	struct s_envp	*next;
 }				t_envp;
 
-t_envp *get_envp(char **envp)
-{
-    t_envp  *head;
-    t_envp  *current;
-    char    *input;
-    char    *delim;
-    char    *key;
-    char    *value;
-
-    head = NULL;
-    current = NULL;
-    while (*envp)
-    {
-        input = *envp;
-        delim = ft_strchr(input, '=');
-        if (delim)
-            key = ft_strdup(input - delim);
-            value = ft_strdup(delim + 1);
-        free(key);
-        free(value);
-        envp++;
-    }
-    return (head);
-}
-
 t_envp      *create_node(char *key, char *value)
 {
     t_envp  *new_node;
@@ -58,3 +33,38 @@ t_envp      *create_node(char *key, char *value)
 
     return (new_node);
 }
+
+t_envp *get_envp(char **envp)
+{
+    t_envp  *head;
+    t_envp  *current;
+    t_envp  *new_node;
+    char    *input;
+    char    *delim;
+    char    *key;
+    char    *value;
+
+    head = NULL;
+    current = NULL;
+    while (*envp)
+    {
+        input = *envp;
+        delim = ft_strchr(input, '=');
+        if (delim)
+        {
+            key = ft_strndup(input, input - delim);
+            value = ft_strdup(delim + 1);
+            new_node = create_node(key, value);
+            if (!head)
+                head = new_node;
+            else
+            current->next = new_node;
+            free(key);
+            free(value);
+        }
+        envp++;
+    }
+    return (head);
+}
+
+
