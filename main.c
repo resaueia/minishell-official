0efa04c501243d0e78cadd27d668eff3fdef6f30
JuliaParnahyba <prog.jump@gmail.com>
2024-06-10 22:32:54 -0300

    pre-main, 1st traying, not finished yet

--- main.c
+++ main.c
@@ -3,53 +3,109 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rsaueia- <rsaueia-@student.42.rio>         +#+  +:+       +#+        */
+/*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/28 18:56:42 by rsaueia-          #+#    #+#             */
-/*   Updated: 2024/06/04 17:39:18 by rsaueia-         ###   ########.fr       */
+/*   Created: 2024/06/10 21:02:03 by jparnahy          #+#    #+#             */
+/*   Updated: 2024/06/10 22:26:42 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int		ft_strlen(char *str)
+void	handle_signals(int sign)
 {
-	int i;
-
-	i = 0;
-	while(str[i])
-		i++;
-	return (i);
-}
-
-void handle_signals(int signo) {
-    if (signo == SIGINT) {
-        printf("\nminishell> ");
-        fflush(stdout);
-    }
-}
-
-
-void	prompt()
-{
-	char	*input;
-
-	while ((input = readline("minishell> ")) != NULL)
+	if (sign == SIGINT)
 	{
-		if (ft_strlen(input) > 0)
-		{
-			add_history(input);
-		//	execute_command(input);
-		}
-		free(input);
+		printf("\nminishell> ");
+	}
+	else if (sign == SIGQUIT)
+	{
+		printf("\n");
 	}
 }
 
-int		main()
+int	check_command_line(int c)
 {
-	signal(SIGINT, handle_signals);
-	signal(SIGQUIT, SIG_IGN);
+	if (c > 1)
+	{
+		printf("Error: too many arguments\n");
+		return (0);
+	}
+	return (1);
+}
 
-	prompt();
+int	ft_strcmp(char *str, char *value)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == value[i] && str[i] && value[i])
+		i++;
+	if (value[i] == '\0')
+		return (0);
+	else
+		return (1);
+}
+
+void	ft_putchar_str(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		printf("str[%i] = %c\n", i, str[i++]);
+	printf("------ is done ------\n");
+}
+
+void	execute_command(char *str)
+{
+	ft_putchar_str(str);
+}
+
+int	main(int c, char *v, char *env[])
+{
+	char *input;
+
+	if (!check_command_line(c))
+		return (0);
+	(void)v;
+	(void)env;
+	// for signal handlin, like ctrl+c and ctrl+d
+	signal(SIGINT, handle_signals);
+	signal(SIGQUIT, handle_signals);
+	// looping the shell
+	while (1)
+	{
+		// the prompt
+		input = readline("minishell> ");
+		// check if input is not null or empty or full of spaces
+		if (input && *input == '\0')
+		{
+			add_history(input);
+			continue ;
+		}
+		if (ft_strcmp(input, "exit") == 0)
+		{
+			free(input);
+			return (0);
+		}
+		else
+		{
+			add_history(input);
+			// execute the command
+			execute_command(input);
+		}
+		free(input);
+	}
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -3,32 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rsaueia- <rsaueia-@student.42.rio>         +#+  +:+       +#+        */
+/*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:51:08 by rsaueia-          #+#    #+#             */
-/*   Updated: 2024/06/04 16:52:52 by rsaueia-         ###   ########.fr       */
+/*   Updated: 2024/06/10 22:21:47 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
 
 /* FUNCTION PROTOTYPES */
-void	prompt();
+void	prompt(void);
 void	execute_command(char *cmd);
 void	handle_signals(int signo);
+int		check_command_line(int c);
 
 /* Built-in commands */
 int		builtin_cd(char **args);
@@ -40,4 +41,3 @@ int		builtin_env(char **args);
 int		builtin_exit(char **args);
 
 #endif
-
