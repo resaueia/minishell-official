d1959a5ee92a2bf57fe160d5bf2ea0eed0d649ba
JuliaParnahyba <prog.jump@gmail.com>
2024-12-17 21:22:54 -0300

    expander is real done

--- expander.c
+++ expander.c
@@ -6,54 +6,15 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 23:43:15 by jparnahy          #+#    #+#             */
-/*   Updated: 2024/12/17 16:53:26 by jparnahy         ###   ########.fr       */
+/*   Updated: 2024/12/17 21:22:39 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/*static char *expander_or_not(char *cmd, t_envp *env_list, int last_exit_status)
-{
-    printf("\n----\non expander_or_not\n");
-    printf("cmd: [%s]\n", cmd);
-    char    *temp;
-    char    *expanded;
-    char    *key;
-    char    *suffix;
-    
-    temp = ft_strdup(cmd);
-    if (temp[0] == '\"') //se começar com aspas duplas e estiver fechada, remover e continuar para expandir
-        remove_quotes(&temp);
-    else if (temp[0] == '\'') //se começar com aspas simples e estiver fechada, remover aspas apenas e não expandir
-    {
-        remove_quotes(&temp);
-        cmd = ft_strdup(temp);
-        temp = free_char_ptr(temp);
-        return (cmd);
-    }
-    if (temp[0] == '$') //se começar com $, verificar a str completa
-    {
-        temp++; //incrementing the pointer to the next character for check next conditions
-		if (*temp == '\0') //if cmd come with $ and no args, it will print just a char '$'
-			temp = cmd; //changing the pointer to the char '$'
-		else //if cmd come with $ and args, need check what kind of args it is
-		{
-            // Caso especial para `$?`
-            if (temp[0] == '?') 
-            {
-                char *status_str = ft_itoa(last_exit_status); // Converte o status para string
-                cmd = ft_strdup(status_str); 
-                free(status_str);
-                free(temp - 1); // Liberar a memória original de temp
-                return (cmd);
-            }
-            if ((ft_isalpha(temp[0]) == 0) && ft_strncmp(temp, "_", 1)) //if echo come with $ and args in lower case, it will print just a newline
+/*          if ((ft_isalpha(temp[0]) == 0) && ft_strncmp(temp, "_", 1)) //if echo come with $ and args in lower case, it will print just a newline
 			{
-                if(ft_strncmp(temp, "\'", 1) == 0 || ft_strncmp(temp, "\"", 1) == 0)
-                    remove_quotes(&temp);
-                else if (temp[0] == '0')
-                    temp = ft_strjoin("bash", temp);
-                else if (temp[0] == '-')
+                
+                else if (temp[0] == '-' || temp[0] == '_')
                     temp = ft_strjoin("himBHs", temp + 1);
                 else if (ft_isdigit(temp[0]) || is_special(temp[0]))
                     temp++;
@@ -105,6 +66,11 @@ static void rmv_db_qts(char **str)
 
     while (*src)
     {
+        if (*src == '$')
+        {
+            if (*(src + 1) == '\"')
+                src++;
+        }
         if (*src != '\"')
             *dst++ = *src;
         src++;
@@ -122,6 +88,11 @@ static void rmv_sg_qts(char **str)
 
     while (*src)
     {
+        if (*src == '$')
+        {
+            if (*(src + 1) == '\'')
+                src++;
+        }
         if (*src != '\'')
             *dst++ = *src;
         src++;
@@ -144,13 +115,6 @@ static void remove_backslashes(char *str)
     }
     *dst = '\0';
 }
-
-/*static int  char_valid(char c)
-{
-    if (char valido)
-        return (1);
-    return (0);
-}*/
 
 char *ft_strjoin_three(char *s1, char *s2, char *s3)
 {
@@ -190,20 +154,16 @@ static char *status_expander(char *str, int i, int exit_status)
 
 static int validate_before_dollar(char *str, int i)
 {
-    if (i > 0)
+    if (str[i - 1] == '&' || str[i - 1] == '!')
+        return 0; // Não expandir
+    if (str[i - 1] == '(' || str[i - 1] == ')')
     {
-        if (str[i - 1] == '&' || str[i - 1] == '!')
-            return 0; // Não expandir
-        if (str[i - 1] == '(' || str[i - 1] == ')')
-        {
-            fprintf(stderr, "bash: syntax error near unexpected token `)'\n");
-            return 0;
-        }
-        if (str[i - 1] == ';')
-        {
-            printf("%.*s\n%s: command not found\n", i, str, str + i + 1);
-            return 0;
-        }
+        printf("minishell: syntax error near unexpected token");
+        if (str[i - 1] == '(')
+            printf(" `%s'\n", &str[i]);
+        else if (str[i - 1] == ')')
+            printf(" `%c'\n", str[i - 1]);
+        return 0;
     }
     return 1;
 }
@@ -218,21 +178,22 @@ static char *handle_invalid_prefix(char *str, int i)
 }
 
 static int handle_special_cases(char *str, int i)
-{
-    char    *pid_str;
-    
+{   
+    int pid;
     if (str[i + 1] == '$') // Substituir por PID
     {
-        pid_str = ft_itoa(getpid());
-        printf("%s\n", pid_str);
-        free(pid_str);
-        return -1; // Caso especial tratado
+        pid = getpid();
+        return (pid); // Caso especial tratado
     }
-    return i + 1;
+    return (-1);
 }
 
 static char *extract_suffix(char *str, int *j)
 {
+    if (str[*j] == '-' || str[*j] == '_')
+        return ft_strdup("himBHs");
+    else if (str[*j] == '0')
+        return ft_strdup("bash");
     while (ft_isalnum(str[*j]) || str[*j] == '_')
         (*j)++;
     return ft_strdup(str + *j);
@@ -243,25 +204,47 @@ static char *get_expanded_value(char *str, int start, t_envp *env_list, int end)
     char *key;
     char *value;
 
-    key = ft_substr(str, start, end - start);
-    remove_backslashes(key);
-    value = get_value(key, env_list);
+    if (ft_isdigit(str[start]) || is_special(str[start]))
+    {
+        start++;
+        if (start == end)
+            return ft_strdup("");
+        else
+        {
+            key = ft_substr(str, start, end - start);
+            value = ft_strdup(key);
+        }
+    }
+    else
+    {
+        key = ft_substr(str, start, end - start);
+        if (is_key(key, env_list))
+            value = get_value(key, env_list);
+        else
+            value = ft_strdup("");    
+    }
     free(key);
-    return value ? value : "";
+    return (value);
 }
 
-static char *rebuild_expanded_str(char *prefix, char *expanded, char *suffix, char *old_str)
+static char *rebd_str(char *prefix, char *expanded, char *suffix, char *old)
 {
-    char *new_str;
-    char *temp;
+    char    *new_str;
+    char    *temp;
+    char    *pre_temp;
 
-    temp = ft_strjoin(prefix, expanded);
-    new_str = ft_strjoin(temp, suffix);
-
+    pre_temp = prefix;
+    if (pre_temp[0] == '*')
+        pre_temp++;
+    temp = ft_strjoin(pre_temp, expanded);
+    if (*pre_temp == '#')
+        new_str = ft_strdup("");
+    else
+        new_str = ft_strjoin(temp, suffix);
     free(temp);
     free(prefix);
     free(suffix);
-    free(old_str);
+    free(old);
     return new_str;
 }
 
@@ -271,25 +254,29 @@ static char *env_var_expander(char *str, int i, t_envp *env_list)
     char *prefix;
     char *suffix;
     char *expanded;
+    int  pid;
     int  j;
 
     if (!validate_before_dollar(str, i))
         return handle_invalid_prefix(str, i);
-    j = handle_special_cases(str, i); // Valida após o dólar
-    if (j == -1)
-        return str; // Caso especial tratado (como pid ou erro)
-
     prefix = ft_substr(str, 0, i); // Parte antes do $
-    suffix = extract_suffix(str, &j); // Sufixo após a variável
-    expanded = get_expanded_value(str, i + 1, env_list, j); // Expande a variável
-    
-    return rebuild_expanded_str(prefix, expanded, suffix, str);
+    pid = handle_special_cases(str, i); // Valida após o dólar
+    if (pid == -1)
+    {
+        j = i + 1;
+        suffix = extract_suffix(str, &j); // Sufixo após a variável
+        expanded = get_expanded_value(str, i + 1, env_list, j); // Expande a variável
+    }
+    else
+    {
+        suffix = ft_strdup(str + i + 2); // Sufixo após o PID
+        expanded = ft_itoa(pid); // Expande o PID
+    }
+    return rebd_str(prefix, expanded, suffix, str);
 }
 
 static char *to_expander(char *str, int i, t_envp *env, int exit_status)
 {
-    //printf("\non to_expander\n");
-    //printf("str: [%s]\n--\n", str);
     char    *expanded;
 
     i = 0;
@@ -297,21 +284,16 @@ static char *to_expander(char *str, int i, t_envp *env, int exit_status)
     {
         if (str[i] == '$')
         {
-            /*if (char_valid(str[i - 1])) //verificar char valido antes do $
-            {
-                i++;
-                continue ;
-            }*/
             if (str[i + 1] == '?')
             {
-                //EXPANDIR EXIT_STATUS
                 expanded = status_expander(str, i, exit_status);;
                 return (expanded);
             }
             else if (str[i + 1] != '\0' && str[i + 1] != '?')
             {
-                //EXPANDIR VARIAVEL DE AMBIENTE
                 expanded = env_var_expander(str, i, env);
+                if (*expanded == '(' || *expanded == ')')
+                    return (ft_strdup(""));
                 return (expanded);
             }
         }
@@ -322,38 +304,35 @@ static char *to_expander(char *str, int i, t_envp *env, int exit_status)
 
 static char *expander_or_not(char *cmd, t_envp *env_list, int exit_status)
 {
-    //printf("\n----\non expander_or_not\n");
-    //printf("cmd: [%s]\n", cmd);
     int     i;
 
     i = 0;
+    remove_backslashes(cmd);
     while(cmd[i])
     {
         if (cmd[i] == '\"') //se começar com aspas duplas e estiver fechada, remover e continuar para expandir
+        {
+            rmv_db_qts(&cmd);
             break;
+        }
         else if (cmd[i] == '\'')//se tiver com aspas simples e estiver fechada, remover aspas apenas e não expandir
         {
-            printf("has single quotes\n");
             rmv_sg_qts(&cmd);
             return (cmd);
         }
         i++;
     }
-    i = 0;
-    while(cmd[i])
-    {
-        if (cmd[i] == '\"') //se começar com aspas duplas e estiver fechada, remover e continuar para expandir
-            rmv_db_qts(&cmd);
+    i = -1;
+    while(cmd[++i])
+    { 
         if (cmd[i] == '$')
             cmd = to_expander(cmd, i, env_list, exit_status);
-        i++;
     }
     return (cmd);
 }
 
 void    lets_expander(t_types *types, t_envp *env_list, int exit_status)
 {
-    //printf("\n----\non lets_expander\n");
     t_envp  *env;
 
     env = env_list;
