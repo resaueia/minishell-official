a66e74289aaca7608ce31b60dd3fd8e88fdf2235
Renan <renansaueia@gmail.com>
2025-01-07 20:15:04 -0300

    uploading refactoring of functions contained in files exec_utils.c cd. exec.c

--- exec.c
+++ exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
+/*   By: rsaueia- <rsaueia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 20:50:29 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/01/06 17:31:16 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/01/07 20:07:19 by rsaueia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@ static void rotate_list(t_types **head)
 			rotate_list(head);
 			return;
 		}
-
 		current = current->next;
 	}
 }
@@ -129,7 +128,7 @@ void	execute_command(t_types *type, t_envp *env_list,
 }
 
 
-int	to_exec(t_init_input *input_list, t_types *type, t_envp *env_list)
+/*int	to_exec(t_init_input *input_list, t_types *type, t_envp *env_list)
 {
 	char	**env;
 
@@ -155,10 +154,58 @@ int	to_exec(t_init_input *input_list, t_types *type, t_envp *env_list)
 	clear_heredoc_files();
 	fd_closer(input_list, type);
 	if (input_list)
-	{
 		free_list(input_list);
-	}
 	if (type)
 		free_types(&type);
+	return (0);
+}*/
+
+static void	free_remaining(t_init_input *input_list, t_types *type)
+{
+	if (input_list)
+		free_list(input_list);
+	if (type)
+		free_types(&type);
+}
+
+static int	process_execution_steps(t_init_input *input_list, t_types *type, t_envp *env_list)
+{
+	char	**env;
+
+	env = NULL;
+	if (is_hdoc(type))
+	{
+		rotate_list(&type);
+		if (handle_heredoc(input_list, type) == -1)
+			return (-1);
+	}
+	if (is_rdrct(type) && handle_redirection(input_list, type) == -1)
+		return (-1);
+	if (is_pp(type))
+		return (handle_pipeline(input_list, env_list, type));
+	if (is_btin(type))
+		execute_builtin(env_list, input_list, type);
+	if (is_exec(type))
+	{
+		env = env_to_char(env_list);
+		execute_command(type, env_list, input_list, env);
+		env = free_from_split(env);
+	}
+	clear_heredoc_files();
+	fd_closer(input_list, type);
+	free_remaining(input_list, type);
+	return (0);
+}
+
+int	to_exec(t_init_input *input_list, t_types *type, t_envp *env_list)
+{
+	if (process_execution_steps(input_list, type, env_list) == -1)
+		return (-1);
+	//clear_heredoc_files();
+	//fd_closer(input_list, type);
+	//if (input_list)
+	//	free_list(input_list);
+	//if (type)
+	//	free_types(&type);
 	return (0);
 }
